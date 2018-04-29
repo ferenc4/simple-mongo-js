e5b2dc64a2f1cc6ff78f3aaaef1b127fac7ae470
Ferenc <ferenc4.fazekas@gmail.com>
2018-04-29 21:58:44 +1000

    added group export

--- models/message.js
+++ models/message.js
@@ -18,33 +18,31 @@ let messageSchema = mongoose.Schema({
 
 let Message = module.exports = mongoose.model('Message', messageSchema);
 
-// Testing only - not cached
-// Get all messages
-module.exports.getMessages = function (callback, limit) {
-    Message.find(callback).limit(limit);
-};
-
-// Add message
-module.exports.addMessage = function (message, callback) {
-    Message.create(message, callback);
-};
-
-// Update message
-module.exports.updateMessage = function (id, message, options, callback) {
-    let query = {_id: id};
-    let update = {
-        message: message
-    };
-    Message.findOneAndUpdate(query, update, options, callback);
-};
-
-// Get message by id
-module.exports.getMessageById = function (id, callback) {
-    Message.findById(id, callback);
-};
-
-// Delete message
-module.exports.deleteMessage = function (id, callback) {
-    let query = {_id: id};
-    Message.deleteOne(query, callback);
+module.exports = {
+    // Testing only - not cached
+    // Get all messages
+    getMessages: (callback, limit) => {
+        Message.find(callback).limit(limit);
+    },
+    // Add message
+    addMessage: (message, callback) => {
+        Message.create(message, callback);
+    },
+    // Update message
+    updateMessage: (id, message, options, callback) => {
+        let query = {_id: id};
+        let update = {
+            message: message
+        };
+        Message.findOneAndUpdate(query, update, options, callback);
+    },
+    // Get message by id
+    getMessageById: (id, callback) => {
+        Message.findById(id, callback);
+    },
+    // Delete message
+    deleteMessage: (id, callback) => {
+        let query = {_id: id};
+        Message.deleteOne(query, callback);
+    }
 };
